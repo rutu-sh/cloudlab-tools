3d2b1e1babb727f2ecf9fd7a323c0b079fa399e6
rutu-sh <ruturajs10@hotmail.com>
2024-06-17 09:26:07 -0400

    feat: changes to make makefile importable

---
+++ cloudlab_tools.mk
@@ -0,0 +1,31 @@
+ifeq ($(CL_CONFIG_PATH),)
+include cloudlab_config.mk
+else
+include $(CL_CONFIG_PATH)
+endif
+
+CLOUDLAB_HOST=$($(NODE))
+REMOTE_DIR?=~/src
+
+cl-verify:
+	@ # check if the CLOUDLAB_USERNAME is set
+	@if [ -z ${CLOUDLAB_USERNAME} ]; then \
+		echo "CLOUDLAB_USERNAME is not set"; \
+		exit 1; \
+	fi
+	@ # check if the NODE is set to a valid node
+	@if [ -z ${CLOUDLAB_HOST} ]; then \
+		echo "NODE is not set to a valid node"; \
+		exit 1; \
+	fi
+
+cl-sync-code: cl-verify
+	@echo "Syncing code to the cloudlab server..."
+	rsync -havpP -e "ssh -i '${SSH_KEY_PATH}'" --exclude .git --exclude .venv \
+		--exclude .vscode ${CURDIR} ${CLOUDLAB_USERNAME}@${CLOUDLAB_HOST}:${REMOTE_DIR} && \
+	echo "Code synced to the cloudlab server"
+
+
+cl-ssh-host: cl-verify
+	@echo "Connecting to the cloudlab host..."
+	ssh -i ${SSH_KEY_PATH} ${CLOUDLAB_USERNAME}@${CLOUDLAB_HOST}
